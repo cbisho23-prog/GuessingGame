dcb2fdbd63a2c00b42ab837ee97b96219389e92b
cablehead0x194 <caleb.b.bishop@gmail.com>
2026-05-21 15:51:39 -0500

    Corrected syntax error

--- src/test/java/GameEngineTest.java
+++ src/test/java/GameEngineTest.java
@@ -80,6 +80,7 @@ public class GameEngineTest {
         engine.setTarget(50);
         engine.makeGuess(-1);
         assertEquals(0, engine.getAttempts());
+    }
 
     @Test
     public void testHintVeryClose() {
